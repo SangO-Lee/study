a2275e103903d9af67ecde5f01c10d3ea3bc3733
Brann lee <ras301@naver.com>
2023-04-08 16:35:06 +0900

    add login

--- vanilla_js/app.js
+++ vanilla_js/app.js
@@ -8,12 +8,12 @@ const USERNAME_KEY = 'username';
 function onLoginSubmit(e){
     e.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    const username = loginInput.value;
-    localStorage.setItem(USERNAME_KEY, username);
-    paintGreetings(username);
+    localStorage.setItem(USERNAME_KEY, loginInput.value);
+    paintGreetings();
 }
 
-function paintGreetings(username){
+function paintGreetings(){
+    const username = localStorage.getItem(USERNAME_KEY);
     greeting.innerText = `Hello ${username}!`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
