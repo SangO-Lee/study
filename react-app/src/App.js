34669c8b58300ebae42f613343a0b6f938cbf652
Brann lee <gkswldn01@naver.com>
2024-07-13 23:24:21 +0900

    clean up function

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,41 +1,31 @@
 import { useState, useEffect } from "react";
-import Button from "./Button";
-import styles from "./App.module.scss";
+
+function Hello() {
+    // function byeFnct = () => {
+    //     console.log("destroyed :(");
+    // }
+    // function hiFnct = () => {
+    //     console.log("Created :)");
+    //     return byeFnct;
+    // }
+    // useEffect(hiFnct,[]);
+    useEffect(() => {
+        console.log("Created :)");
+        return () => {
+            console.log("Destroyed :(");
+        }; //component가 사라질때 동작하는 cleanup funtion.
+    }, []);
+
+    return <h1>Hello</h1>;
+}
 
 function App() {
-    const [counter, setValue] = useState(0);
-    const [keyword, setKeyword] = useState("");
-    const onClick = () => setValue((prev) => prev + 1);
-    const onChange = (event) => {
-        setKeyword(event.target.value);
-    };
-
-    // console.log("I run all the time");
-    useEffect(() => {
-        // useEffect는 react.js에 내장된 함수로. 특정 함수를 최초 한번만 호출해야할 때 사용함.
-        console.log("COUNTER CHANGES");
-    }, [counter]);
-    useEffect(() => {
-        if (keyword !== "" && keyword.length > 5) {
-            console.log("SEARCH FOR..", keyword);
-        }
-    }, [keyword]); // keyword가 변할때만 이 코드를 실행하라는 뜻
-    // [] 안에는 실행 조건이 들어있음
-
-    useEffect(() => {
-        console.log("keyword and counter changes.");
-    }, [keyword, counter]);
-    //여러개의 변화를 동시에 감지하는 것도 가능하다.
+    const [showing, setShowing] = useState(false);
+    const onClick = () => setShowing((prev) => !prev);
     return (
         <div>
-            <input
-                value={keyword}
-                onChange={onChange}
-                type="text"
-                placeholder="Search here..."
-            ></input>
-            <h1 className={styles.title}>{counter}</h1>
-            <Button text={"click me"} onClick={onClick} />
+            <button onClick={onClick}>{!showing ? "Show" : "Hide"}</button>
+            {showing ? <Hello></Hello> : null}
         </div>
     );
 }
