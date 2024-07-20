c237e97cb64737bf782ce92dbfaea301521eaf42
Brann lee <gkswldn01@naver.com>
2024-07-20 18:08:48 +0900

    movie id 파라미터 추가

--- react-app/src/components/Movie.js
+++ react-app/src/components/Movie.js
@@ -1,12 +1,12 @@
 import { PropTypes } from "prop-types";
 import { Link } from "react-router-dom";
 
-function Movie({ coverImg, title, summary, genres }) {
+function Movie({ coverImg, title, summary, genres, id }) {
     return (
         <div>
             <img src={coverImg} />
             <h2>
-                <Link to="/movie">&lt; {title} &gt;</Link>
+                <Link to={`/movie/${id}`}>&lt; {title} &gt;</Link>
             </h2>
             <p>{summary}</p>
             <ul>{genres.map((g) => [<li key={g}>{g}</li>])}</ul>
@@ -19,5 +19,6 @@ Movie.propTypes = {
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
     genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+    id: PropTypes.number.isRequired,
 };
 export default Movie;
